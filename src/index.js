fc9f4d0d76f434bea581b08fa6c3680e16cfb75f
KatsiarynaZaviadzeyeva <katsiarynaappleby@gmail.com>
2023-01-19 23:16:45 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+//function reverse(n) {
+    let positiveNumber = Math.abs(n)
+    console.log(positiveNumber);
+    let string = String(positiveNumber);
+    console.log(string);
+    return Number(string.split('').reverse().join(''));
 }
+//console.log(reverse(-100));
+
